9da8612201d5fdb20ef8f261cc601994554ed1c5
Ashish Bhatia <ashishb@axsource.com>
2026-05-16 05:50:22 +0000

    Add authentication settings model

---
+++ ai-gateway-api/Models/AuthSettings.cs
@@ -0,0 +1,13 @@
+namespace CommerceAIAgents.Models;
+
+public class AuthSettings
+{
+    public string TenantId { get; set; }
+        = "";
+
+    public string ClientId { get; set; }
+        = "";
+
+    public string ClientSecret { get; set; }
+        = "";
+}
